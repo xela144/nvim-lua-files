153541ebe846cd933ac3748ed16e4945a2373c49
xela144 <alex.d.bardales@gmail.com>
2023-06-20 19:25:25 -0700

    Remove some nvim-tree mappings

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,4 +1,22 @@
-require("nvim-tree").setup({
+local nvimtree = require("nvim-tree")
+
+-- Create a local "on_attach" to edit the context menu "mappings"
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- Remove the "Run System"
+  vim.keymap.del("n", "s", { buffer = bufnr })
+
+  -- Remove the "Open in place"
+  vim.keymap.del("n", "<C-E>", { buffer = bufnr })
+
+end
+
+
+nvimtree.setup({
+  on_attach = my_on_attach,
   renderer = {
     icons = {
       show = {
