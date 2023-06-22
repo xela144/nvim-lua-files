f5f60dab6dce7fdcd7f6613cc3fcf7ca09082db4
xela144 <alex.d.bardales@gmail.com>
2023-06-22 09:01:13 -0700

    comment/whitespace

--- lua/alex/remap.lua
+++ lua/alex/remap.lua
@@ -1,11 +1,10 @@
---vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
-
--- Navigate buffers with ctrl-jkhl
+-- Navigate windows with ctrl-jkhl
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 
+-- Move visual blocks and preserve indentation w.r.t. control blocks
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
