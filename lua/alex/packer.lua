5cf07c1c77ff25dbdcd66c3fe353c4ddab584293
Alex Bardales <abardales@picarro.com>
2025-02-14 14:10:18 -0800

    Adding treesitter-context, and formatting

--- lua/alex/packer.lua
+++ lua/alex/packer.lua
@@ -1,72 +1,72 @@
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  use("wbthomason/packer.nvim")
 
   use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    -- or                            , branch = '0.1.x',
+    requires = { { "nvim-lua/plenary.nvim" } },
   })
-  use('nvim-neotest/nvim-nio')
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate,:TSInstall python' })
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
+
+  use({
+    "rose-pine/neovim",
+    as = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  })
+  use("nvim-neotest/nvim-nio")
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate,:TSInstall python" })
+  use("nvim-treesitter/nvim-treesitter-context")
+  use("nvim-treesitter/playground")
+  use("theprimeagen/harpoon")
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" }, -- Required
+      { -- Optional
+        "williamboman/mason.nvim",
+        run = function()
+          pcall(vim.cmd, "MasonUpdate")
+        end,
+      },
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" }, -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "L3MON4D3/LuaSnip" }, -- Required
     },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  })
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+  -- Nvim-tree to replace NerdTree
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+  })
 
+  -- Useless plugin
+  use({ "eandrju/cellular-automaton.nvim" })
 
--- Nvim-tree to replace NerdTree
-use {
-  'nvim-tree/nvim-tree.lua',
-  requires = {
-    'nvim-tree/nvim-web-devicons', -- optional
-  },
-}
+  -- Debugger
+  use({ "mfussenegger/nvim-dap" })
+  -- Python, Go
+  use({ "mfussenegger/nvim-dap-python" })
+  use({ "leoluz/nvim-dap-go" })
 
--- Useless plugin
-use {'eandrju/cellular-automaton.nvim'}
+  -- Debugger UI
+  use({ "rcarriga/nvim-dap-ui", requires = "mfussenegger/nvim-dap" })
 
--- Debugger
-use {'mfussenegger/nvim-dap'}
--- Python, Go
-use {'mfussenegger/nvim-dap-python'}
-use {'leoluz/nvim-dap-go'}
-
--- Debugger UI
-use {'rcarriga/nvim-dap-ui', requires='mfussenegger/nvim-dap'}
-
--- Formatter
-use {'mhartington/formatter.nvim'}
-
+  -- Formatter
+  use({ "mhartington/formatter.nvim" })
 end)
