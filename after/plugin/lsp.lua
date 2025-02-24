f11180574cde729ad17ab7d885ac51b590e73e85
Alex Bardales <abardales@picarro.com>
2025-02-23 17:37:17 -0800

    formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,72 +1,107 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
-lsp.preset('recommended')
-require('mason').setup({
-    ensure_installed = {
-        'prettierd',
-        'gopls',
-    }
+lsp.preset("recommended")
+require("mason").setup({
+  ensure_installed = {
+    "prettierd",
+    "gopls",
+  },
 })
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'lua_ls',
-        'tailwindcss',
-        'tsserver',
-    },
-    handlers = {
-      function(server_name)
-        require('lspconfig')[server_name].setup({})
-      end,
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "tailwindcss",
+    -- 'tsserver',
+  },
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
   },
 })
 
-
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-    })
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+  }),
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = "E",
+    warn = "W",
+    hint = "H",
+    info = "I",
+  },
 })
 
-lsp.on_attach(
-    function(client, bufnr)
-        local opts = {buffer = bufnr, remap = false}
+lsp.on_attach(function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("n", "<leader>vsh", function() vim.lsp.buf.signature_help() end, opts)
-    end
-)
+  vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition()
+  end, opts)
+  vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover()
+  end, opts)
+  vim.keymap.set("n", "<leader>vws", function()
+    vim.lsp.buf.workspace_symbol()
+  end, opts)
+  vim.keymap.set("n", "<leader>vd", function()
+    vim.diagnostic.open_float()
+  end, opts)
+  vim.keymap.set("n", "[d", function()
+    vim.diagnostic.goto_next()
+  end, opts)
+  vim.keymap.set("n", "]d", function()
+    vim.diagnostic.goto_prev()
+  end, opts)
+  vim.keymap.set("n", "<leader>vca", function()
+    vim.lsp.buf.code_action()
+  end, opts)
+  vim.keymap.set("n", "<leader>vrr", function()
+    vim.lsp.buf.references()
+  end, opts)
+  vim.keymap.set("n", "<leader>vrn", function()
+    vim.lsp.buf.rename()
+  end, opts)
+  vim.keymap.set("n", "<leader>vsh", function()
+    vim.lsp.buf.signature_help()
+  end, opts)
+end)
+
+lsp.configure("pylsp", {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          maxLineLength = 100,
+          ignore = { "W391" },
+          enabled = true,
+        },
+        flake8 = {
+          maxLineLength = 100,
+          enabled = true,
+        },
+      },
+    },
+  },
+})
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true,
 })
