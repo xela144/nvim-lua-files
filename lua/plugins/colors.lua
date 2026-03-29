f61b40babf67aafdd01f5260d5c9f61b75f1c3ab
Alex Bardales <alex.d.bardales@gmail.com>
2026-03-29 12:25:02 -0700

    Color scheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -8,9 +8,7 @@ return {
       })
 
       function ColorMyPencils(color)
-        color = color or "rose-pine"
-        vim.cmd.colorscheme(color)
-
+        vim.cmd("colorscheme rose-pine-moon")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
       end
