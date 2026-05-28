20fc6c6402c2aa6d7573164b832a1bda19f2e844
xela144 <alex.d.bardales@gmail.com>
2026-05-27 18:25:57 -0700

    mostly formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,45 +1,45 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = {
-        formatters_by_ft = {
-            javascript = { "biome" },
-            typescript = { "biome" },
-            javascriptreact = { "biome" },
-            typescriptreact = { "biome" },
-            json = { "biome" },
-            css = { "biome" },
-            python = { "ruff_format" },
-        },
-
-        format_on_save = function(bufnr)
-            -- Disable with a global or buffer-local variable
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-            end
-            return { timeout_ms = 500, lsp_fallback = true }
-        end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "biome" },
+      typescript = { "biome" },
+      javascriptreact = { "biome" },
+      typescriptreact = { "biome" },
+      json = { "biome" },
+      css = { "biome" },
+      python = { "ruff_format" },
     },
-    init = function()
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
-        vim.keymap.set("n", "<leader>fm", function()
-            vim.g.disable_autoformat = not vim.g.disable_autoformat
-            vim.notify("Format on save: " .. (vim.g.disable_autoformat and "off" or "on"),
-                vim.log.levels.INFO)
-        end, { desc = "Toggle format on save" })
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
-        vim.api.nvim_create_user_command("FormatDisable", function(args)
-            if args.bang then
-                vim.b.disable_autoformat = true
-            else
-                vim.g.disable_autoformat = true
-            end
-        end, { desc = "Disable autoformat", bang = true })
-        vim.api.nvim_create_user_command("FormatEnable", function()
-            vim.b.disable_autoformat = false
-            vim.g.disable_autoformat = false
-        end, { desc = "Enable autoformat" })
-    end
+    vim.keymap.set("n", "<leader>fm", function()
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+      vim.notify("Format on save: " .. (vim.g.disable_autoformat and "off" or "on"), vim.log.levels.INFO)
+    end, { desc = "Toggle format on save" })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, { desc = "Disable autoformat", bang = true })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { desc = "Enable autoformat" })
+  end,
 }
