3c6dd7f23671b84af8a4e8689f840f7faf819e75
xela144 <alex.d.bardales@gmail.com>
2026-05-27 18:27:32 -0700

    Use newer treesitter for nvim 12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,19 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = { "markdown" },
-          additional_vim_regex_highlighting = false,
-        },
-      })
-    end,
   },
   "nvim-treesitter/nvim-treesitter-context",
-  "nvim-treesitter/playground",
 }
